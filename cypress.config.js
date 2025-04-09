48cc8de75594a6fdbe55e60a220d659f535b1dbc
Arulprakash <infotecharul@gmail.com>
2025-04-09 12:23:01 +0200

    addrd executable permissions

--- cypress.config.js
+++ cypress.config.js
@@ -7,5 +7,11 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
     experimentalStudio: true, // Enable experimental Studio
+    // Here you can specify other configurations related to e2e
+  },
+  // Removed the invalid syntax and added a proper start command
+  start: {
+    command: "npm start", // Command to start your server
+    background: true, // Run the command in the background
   },
 });
